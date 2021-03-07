1dd4487e5f54bbdc33d6c6c62581974cec0f516c
Jimmy Menjura <jimmymenjura25@hotmail.com>
2021-03-06 21:47:05 -0500

    Se suben todos los archivos del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  map:any;
   title = 'proyectoMapas';
+  constructor(){}
+
+  ngOnInit(){
+    this.verMapa();
+  }
+  verMapa(){
+    const latYlon = {
+      lat:4.6265976722086375,
+      lng:-74.15139124716826,
+    };
+    this.map = L.map('map', {
+      center: [ latYlon.lat,latYlon.lng ],
+      zoom: 13
+    });
+   const mapa = L.tileLayer('	https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',{
+      minZoom:12,
+      maxZoom:17,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    mapa.addTo(this.map)
+  }
 }
